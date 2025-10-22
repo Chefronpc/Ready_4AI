c89b377163fd40c0153abe4b91f218dafbcb8d89
Chefronpc <chefronpc@gmail.com>
2025-10-23 00:20:21 +0200

    feat: odczyt GET i zapis POST

---
+++ Prog_API/api.py
@@ -0,0 +1,46 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/"
+
+while True:
+
+    inp = input("enter -> odczyt WebSerwis\nnext -> następna funkcja\n>> ")
+
+    if inp == "next":
+        break
+    elif inp == "":
+        continue
+
+    response = requests.get(f"{url}{inp}")
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Error: {response.status_code}")
+
+
+while True:
+
+    in_titile  = input("Podaj tytuł: ")
+    in_body    = input("Podaj treść: ")
+
+    if in_titile == "next" or in_body == "next":
+        break
+    elif in_titile == "" or in_body == "":
+        continue
+
+    data = {
+        "title": in_titile,
+        "body": in_body
+    }
+
+    response = requests.post(url, json=data)
+
+    if response.status_code == 201:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Error: {response.status_code}")
+
+
