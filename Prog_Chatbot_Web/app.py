3348f1dd72dc4f49237f73e7999dd2f6a13909d4
Chefronpc <chefronpc@gmail.com>
2025-11-02 14:51:59 +0100

    feat: Dodaj obsługę endpointu "chat" + zależności "requirments.txt"

--- Prog_Chatbot_Web/app.py
+++ Prog_Chatbot_Web/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+client = OpenAI()
 
 
 @app.route('/chat/hello')
@@ -8,10 +13,35 @@ def Hello_World():
     return 'Hello World!'
 
 
+def send_message(message, previous_response_id):
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=message,
+        previous_response_id=previous_response_id
+    )
+
+    return response.output_text, response.id
+
+
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
-    return jsonify(data)
+
+    if not data or 'message' not in data:
+        return jsonify({'error': 'Missing field(s)'}), 400
+
+
+    message = data.get('message')
+    previous_response_id = data.get('previousResponseId')
+
+    response, response_id = send_message(message, previous_response_id)
+
+    return jsonify(
+        {
+            'response': response,
+            'responseId': response_id
+        }
+    )
 
 
 if __name__ == '__main__':
